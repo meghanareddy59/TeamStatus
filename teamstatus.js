524e0e525048dd0b274495073fbf7b0b6eca3ce7
meghana <meghana.gaddam@pramati.com>
2020-01-19 18:48:10 +0530

    teamstatusui

---
+++ teamstatus.js
@@ -0,0 +1,185 @@
+var bodymain=document.createElement('div');
+bodymain.setAttribute('id','main');
+document.body.appendChild(bodymain);
+
+
+//heading
+var heading=document.createElement('div');
+heading.setAttribute('class','heading');
+heading.textContent="Team status"
+bodymain.appendChild(heading);
+
+// 1st payer
+var img1_span=document.createElement('span');
+img1_span.setAttribute('class','img');
+bodymain.appendChild(img1_span);
+
+//img
+var img1=document.createElement('img');
+img1.setAttribute('class','logo');
+img1.src="https://pbs.twimg.com/profile_images/1142912751687229440/NgNsSU4w_400x400.png"
+img1_span.appendChild(img1);
+//details
+var setrank1=document.createElement("p")
+setrank1.textContent="Position : 1";
+img1_span.appendChild(setrank1);
+
+var setteam1=document.createElement("p");
+setteam1.textContent="Team name : Fire Birds";
+var won1=document.createElement("p");
+won1.textContent="Matches won : 10";
+var lost1=document.createElement("p");
+lost1.textContent="Matches lost : 2";
+var setpoints1=document.createElement("p");
+setpoints1.textContent="Total points : 20";
+
+img1_span.appendChild(setteam1);
+img1_span.appendChild(won1);
+img1_span.appendChild(lost1);
+img1_span.appendChild(setpoints1);
+
+
+
+//2nd player
+var img2_span=document.createElement('span');
+img2_span.setAttribute('class','img');
+bodymain.appendChild(img2_span);
+
+var img2=document.createElement('img');
+img2.setAttribute('class','logo');
+img2.src="http://djnzwlw55sn6l.cloudfront.net/uploads/3d2fa30530f307e164a22192ff4bcff3/IMG-20160106-WA0011.jpg"
+img2_span.appendChild(img2);
+var rank2=document.createElement("p")
+rank2.textContent="Position : 2";
+img2_span.appendChild(rank2);
+var setteam2=document.createElement("p");
+setteam2.textContent="Team name : Smashers";
+var won2=document.createElement("p");
+won2.textContent="Matches won : 9";
+var lost2=document.createElement("p");
+lost2.textContent="Matches lost : 3";
+var setpoints2=document.createElement("p");
+setpoints2.textContent="Total points : 18";
+
+img2_span.appendChild(setteam2);
+img2_span.appendChild(won2);
+img2_span.appendChild(lost2);
+img2_span.appendChild(setpoints2);
+
+var img3_span=document.createElement('span');
+img3_span.setAttribute('class','img');
+bodymain.appendChild(img3_span);
+
+
+var img3=document.createElement('img');
+img3.setAttribute('class','logo');
+img3.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnRbgSrwvCWGFMJZpg7UAaMsAFMJ9u82LH3GOwTgrtdO2WlVo7&s"
+img3_span.appendChild(img3);
+//details
+var setrank3=document.createElement("p")
+setrank3.textContent="Rank : 3";
+var setteam3=document.createElement("p");
+setteam3.textContent="Team name : Sun Risers";
+var won3=document.createElement("p");
+won3.textContent="Matches won : 6";
+var lost3=document.createElement("p");
+lost3.textContent="Matches lost : 4";
+var setpoints3=document.createElement("p");
+setpoints3.textContent="Total points : 12";
+img3_span.appendChild(setrank3);
+
+img3_span.appendChild(setteam3);
+img3_span.appendChild(won3);
+img3_span.appendChild(lost3);
+img3_span.appendChild(setpoints3);
+
+
+
+
+var img4_span=document.createElement('span');
+img4_span.setAttribute('class','img');
+bodymain.appendChild(img4_span);
+
+
+var img4=document.createElement('img');
+img4.setAttribute('class','logo');
+img4.src="https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/0bdd0c56696581.59b8e3d4b1c7c.jpg"
+img4_span.appendChild(img4);
+var setrank4=document.createElement("p")
+setrank4.textContent="Rank : 4";
+var setteam4=document.createElement("p");
+setteam4.textContent="Team name : Sun Risers";
+var won4=document.createElement("p");
+won4.textContent="Matches won : 5";
+var lost4=document.createElement("p");
+lost4.textContent="Matches lost : 5";
+var setpoints4=document.createElement("p");
+setpoints4.textContent="Total points : 10";
+img4_span.appendChild(setrank4);
+
+img4_span.appendChild(setteam4);
+img4_span.appendChild(won4);
+img4_span.appendChild(lost4);
+img4_span.appendChild(setpoints4);
+
+
+
+var img5_span=document.createElement('span');
+img5_span.setAttribute('class','img');
+bodymain.appendChild(img5_span);
+
+
+var img5=document.createElement('img');
+img5.setAttribute('class','logo');
+img5.src="https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/82c61656696581.59b8e3d4adb4d.jpg"
+img5_span.appendChild(img5);
+var rank5=document.createElement("p")
+rank5.textContent="Rank : 5";
+
+
+var setrank5=document.createElement("p")
+setrank5.textContent="Rank : 5";
+img5_span.appendChild(setrank5);
+var setteam5=document.createElement("p");
+setteam5.textContent="Team name : Sun Risers";
+var won5=document.createElement("p");
+won5.textContent="Matches won : 4";
+var lost5=document.createElement("p");
+lost5.textContent="Matches lost : 6";
+var setpoints5=document.createElement("p");
+setpoints5.textContent="Total points : 8";
+
+img5_span.appendChild(setteam5);
+img5_span.appendChild(won5);
+img5_span.appendChild(lost5);
+img5_span.appendChild(setpoints5);
+
+
+var img6_span=document.createElement('span');
+img6_span.setAttribute('class','img');
+bodymain.appendChild(img6_span);
+
+
+var img6=document.createElement('img');
+img6.setAttribute('class','logo');
+img6.src="https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/47f53d56696581.59b8e3d4aee3f.jpg"
+img6_span.appendChild(img6);
+var setrank6=document.createElement("p")
+setrank6.textContent="Rank : 6";
+
+
+var setteam6=document.createElement("p");
+setteam6.textContent="Team name : Sun Risers";
+var won6=document.createElement("p");
+won6.textContent="Matches won : 3";
+var lost6=document.createElement("p");
+lost6.textContent="Matches lost : 7";
+var setpoints6=document.createElement("p");
+setpoints6.textContent="Total points : 6";
+img6_span.appendChild(setrank6);
+
+img6_span.appendChild(setteam6);
+img6_span.appendChild(won6);
+img6_span.appendChild(lost6);
+img6_span.appendChild(setpoints6);
+
